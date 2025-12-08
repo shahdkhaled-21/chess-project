4b6d5190d8c21530b2dc4069e9fc54b75e1216c8
Shams Alaa Ali Ghobashy <es.shams.alaa2024@alexu.edu.eg>
2025-12-08 20:13:16 +0200

    Added queen move moves.c

--- moves.c
+++ moves.c
@@ -24,4 +24,152 @@ void knight(int i, int j, int r, int c){
         printf("Invalid move");
     }
 }
+void queen(int i, int j, int r, int c){
+    int k=i;int p=j;int emptyPath;
+    if(abs(i-r)==abs(j-c)){
+        if(i>r && j>c){
+            while(k>r && p>c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k--;
+                p--;
+            }
+        }
+        else if(i<r && j<c){
+            while(k<r && p<c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k++;
+                p++;
+            }
+        }
+        else if(i<r && j>c){
+            while(k<r && p>c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k++;
+                p--;
+            }
+        }
+        else if(i>r && j<c){
+            while(k<r && p>c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k--;
+                p++;
+            }
+        }
+        if(emptyPath==1){
+            if(board[r][c]=='-'||board[r][c]=='.'){
+                char temp=board[r][c];
+                board[r][c]=board[i][j];
+                board[i][j]=temp;
+            }
+            else if(board[r][c]==opponent_piece){
+                killed_arr[counter]=board[r][c];
+                counter++;
+                board[r][c]=board[i][j];
+                board[i][j]=board[r-1][c];
+            }
+            else if(board[r][c]==friendly_piece){
+                printf("Cannot eat friendly piece");
+            }
+        }
+        else{
+            printf("Invalid move");
+        }
+    }
+    else if(r==i){
+        if(j>c){
+            while(p>c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                p--;
+            }
+        }
+        else if(j<c){
+            while(p<c){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                p++;
+            }
+        }
+    }
+    else if(c==j){
+        if(i>r){
+            while(k>r){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k--;
+            }
+        }
+        else if(j<c){
+            while(k<r){
+                if(board[k][p]=='.'||board[k][p]=='-'){
+                    emptyPath=1;
+                }
+                else{
+                    emptyPath=0;
+                    break;
+                }
+                k++;
+            }
+        }
+    }
+    if(emptyPath==1){
+        if(board[r][c]=='-'||board[r][c]=='.'){
+            char temp=board[r][c];
+            board[r][c]=board[i][j];
+            board[i][j]=temp;
+        }
+        else if(board[r][c]==opponent_piece){
+            killed_arr[counter]=board[r][c];
+            counter++;
+            board[r][c]=board[i][j];
+            board[i][j]=board[r-1][c];
+        }
+        else if(board[r][c]==friendly_piece){
+            printf("Cannot eat friendly piece");
+        }
+    }
+    else{
+        printf("Invalid move");
+    }
+}
+
 
