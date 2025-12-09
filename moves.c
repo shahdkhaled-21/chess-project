74ac0ea89dc3104783d68bbe3604025065e937a9
Shams Alaa Ali Ghobashy <es.shams.alaa2024@alexu.edu.eg>
2025-12-09 22:16:14 +0200

    Added pawn move moves.c

--- moves.c
+++ moves.c
@@ -14,7 +14,7 @@ void knight(int i, int j, int r, int c){
             killed_arr[counter]=board[r][c];
             counter++;
             board[r][c]=board[i][j];
-            board[i][j]='.';//some modifications must be applied here
+            board[i][j]='.';//some modifications must be applied here******************
         }
         else if(board[r][c]==friendly_piece){
             printf("Cannot eat friendly piece");
@@ -171,6 +171,45 @@ void queen(int i, int j, int r, int c){
         printf("Invalid move");
     }
 }
+void pawn(int i, int j, int r, int c){
+    int firstMove=0;
+    if(board[i][j]==board[1][j] || board[i][j]==board[6][j]){
+        firstMove=1;
+    }
+    if(firstMove==1 && r-i==2)){
+        if(board[r][c]=='-'||board[r][c]=='.'){
+            char temp=board[r][c];
+            board[r][c]=board[i][j];
+            board[i][j]=temp;
+        }
+        else if(board[r][c]==opponent_piece || board[r][c]==friendly_piece){
+            printf("Invalid move");
+        }
+    }
+    else if(r-i==1){
+        if(board[r][c]=='-'){
+            board[r][c]=board[i][j];
+            board[i][j]='.';
+        }
+        else if(board[r][c]=='.'){
+            board[r][c]=board[i][j];
+            board[i][j]='-';
+        }
+        else if(board[r][c]==opponent_piece || board[r][c]==friendly_piece){
+            printf("Invalid move");
+        }
+    }
+    else if(r-i=1 && c-j=1 && board[r][c]==opponent_piece || r-i=-1 && c-j==1 && board[r][c]==opponent_piece){
+            killed_arr[counter]=board[r][c];
+            counter++;
+            board[r][c]=board[i][j];
+            board[i][j]='-';//some modifications must be applied here******************
+    }
+    else{
+        printf("Invalid move");
+    }
+}
+
 
 
 
